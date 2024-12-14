f2623cdd8193a91d50974923d56496aaedb57b7a
naivoder <naivoder@gmail.com>
2024-12-14 15:11:29 -0500

    rsample for imagine policy brings back grad norm 🤔

--- networks.py
+++ networks.py
@@ -269,10 +269,8 @@ class RSSM(nn.Module):
         # Loop through each step, predicting forward in time using the prior.
         for t in range(horizon):
             if actions is None:
-                # with torch.no_grad(): # is this equivalent to state.detach()? 
-                    # print(type(actor))
                 dist = actor(torch.cat(state, dim=-1))
-                action = dist.sample()
+                action = dist.rsample() # maybe this needs to be rsample? 
             else:
                 action = actions[:, t]
             # action = action.unsqueeze(-1)
